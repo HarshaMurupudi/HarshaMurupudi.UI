a418617edf84947e4bf5a55ebcd7659da02f23aa
HarshaMurupudi <itsmeeeharsha@gmail.com>
2024-07-30 11:47:25 +0530

    ci: change deploy script try-3

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { Landing } from '../features/landing';
 
 function index(props) {
-  return <Landing {...props} />;
+  // return <Landing {...props} />;
+  return <div />;
 }
 
 export default index;
