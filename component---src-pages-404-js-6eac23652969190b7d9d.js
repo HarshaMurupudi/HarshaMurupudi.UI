19ac06cbee89eaa6e2940156251a5c549eb53461
HarshaMurupudi <itsmeeeharsha@gmail.com>
2024-08-22 09:23:02 +0530

    Updates

---
+++ component---src-pages-404-js-6eac23652969190b7d9d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),u=n(5930);t.default=()=>l.createElement(u.Z,null,l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-6eac23652969190b7d9d.js.map
